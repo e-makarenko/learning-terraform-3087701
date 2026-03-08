80cb4e0f36b9d81574fe9a4bad5f23fd38319d6f
e-makarenko <159464819+e-makarenko@users.noreply.github.com>
2026-03-08 12:22:23 +0100

    use module for sg

--- main.tf
+++ main.tf
@@ -22,12 +22,28 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
+
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.1"
+  name    = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingess_rules        = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egess_rules        = ["all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
 
 resource "aws_security_group" "blog" {
   name        = "blog"
